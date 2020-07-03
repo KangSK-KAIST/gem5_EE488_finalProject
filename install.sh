5834f0883805cdfdc8c7bedef11e0073876c4de5
KangSK-KAIST <steelyk7@gmail.com>
2020-07-03 18:47:07 +0900

    Added build script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install PATH
-INSTALL_PATH="${HOME}/Documents/KangSK"
+INS_PATH=$(pwd)
 
 # Install packages
 sudo apt update && sudo apt upgrade
@@ -11,6 +11,8 @@ sudo apt install build-essential  scons zlib1g-dev m4 swig python-pip python-vir
 mkdir $HOME/.pyenv; cd $HOME/.pyenv
 sudo virtualenv -p "/usr/bin/python2.7" gem5 &&
 source "gem5/bin/activate" &&
-which python &&
 sudo pip install six scons
 
+# Build
+cd $INS_PATH/gem5
+scons build/ARM/gem5.opt -j $(nproc) --ignore-style
